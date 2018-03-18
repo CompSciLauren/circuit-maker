31d3c052d731f698b4204e0fbe583a4140b08faa
Lauren Stephenson <compscilauren@gmail.com>
2018-03-17 20:10:03 -0500

    Added and gate button

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const innerHeight = window.innerHeight;
 
 // first we need to create a stage
 const stage = new Konva.Stage({
-  container: 'app',   // id of container <div>
+  container: 'canvas',   // id of container <div>
   width: innerWidth,
   height: innerHeight,
 });
@@ -13,7 +13,7 @@ const stage = new Konva.Stage({
 // then create layer
 const layer = new Konva.Layer();
 
-function addGate(x, y) {
+function addAndGate(x, y) {
   const group = new Konva.Group({
     draggable: true,
   });
@@ -42,7 +42,38 @@ function addGate(x, y) {
   layer.add(group);
 };
 
-addGate(200, 200);
+function addOrGate(x, y) {
+  const group = new Konva.Group({
+    draggable: true,
+  });
+  const rectWidth = 100;
+  const rectHeight = 50;
+  const rect = new Konva.Rect({
+    x: x,
+    y: y,
+    width: rectWidth,
+    height: rectHeight,
+    fill: 'orange',
+    stroke: 'black',
+    strokeWidth: 4,
+  });
 
-// add the layer to the stage
-stage.add(layer);
+  const text = new Konva.Text({
+    x: x + 30,
+    y: y + rectHeight / 4,
+    text: 'OR',
+    fontSize: 30,
+    fontFamily: 'Comic Sans',
+    fill: 'black',
+  });
+  group.add(rect);
+  group.add(text);
+  layer.add(group);
+};
+
+function andGateButtonClick() {
+  addAndGate(0, 0);
+  stage.add(layer);
+}
+
+window.andGateButtonClick = andGateButtonClick;
