cc3dd02555132d7fc0a181df63c2a9f21d43dde7
Lauren Stephenson <compscilauren@gmail.com>
2018-03-17 22:43:39 -0500

    Added connector button

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import Konva from 'konva';
 const innerWidth = window.innerWidth;
 const innerHeight = window.innerHeight;
 
+let truePos, falsePos;
+let andPos, orPos;
+let connectorPos;
+
 // first we need to create a stage
 const stage = new Konva.Stage({
   container: 'canvas',   // id of container <div>
@@ -37,6 +41,10 @@ function addTrueButton(x, y) {
     fontFamily: 'Comic Sans',
     fill: 'black',
   });
+  group.on("dragend", function(e){
+    truePos = circle.getAbsolutePosition();
+  });
+
   group.add(circle);
   group.add(text);
   layer.add(group);
@@ -66,6 +74,10 @@ function addFalseButton(x, y) {
     fontFamily: 'Comic Sans',
     fill: 'black',
   });
+  group.on("dragend", function(e){
+    falsePos = circle.getAbsolutePosition();
+  });
+
   group.add(circle);
   group.add(text);
   layer.add(group);
@@ -95,6 +107,10 @@ function addAndGate(x, y) {
     fontFamily: 'Comic Sans',
     fill: 'black',
   });
+  group.on("dragend", function(e){
+    andPos = rect.getAbsolutePosition();
+  });
+
   group.add(rect);
   group.add(text);
   layer.add(group);
@@ -124,18 +140,51 @@ function addOrGate(x, y) {
     fontFamily: 'Comic Sans',
     fill: 'black',
   });
+  group.on("dragend", function(e){
+    orPos = rect.getAbsolutePosition();
+  });
+
   group.add(rect);
   group.add(text);
   layer.add(group);
 };
 
+function addConnectorButton(x, y) {
+  const group = new Konva.Group({
+    draggable: true,
+  });
+  const rectWidth = 150;
+  const rectHeight = 10;
+  const rect = new Konva.Rect({
+    x: x,
+    y: y,
+    width: rectWidth,
+    height: rectHeight,
+    fill: 'orange',
+    stroke: 'black',
+    strokeWidth: 4,
+  });
+
+  group.on("dragend", function(e){
+    connectorPos = rect.getAbsolutePosition();
+
+    if (connectorPos.x >= andPos.x && connectorPos.x <= andPos.x+100) {
+      const groupConnected = new Konva.Group();
+      console.log("Connector and And Gate connected",groupConnected);
+    }
+  });
+
+  group.add(rect);
+  layer.add(group);
+}
+
 function trueButtonClick() {
-  addTrueButton(0, 0)
+  addTrueButton(0, 0);
   stage.add(layer);
 }
 
 function falseButtonClick() {
-  addFalseButton(0, 0)
+  addFalseButton(0, 0);
   stage.add(layer);
 }
 
@@ -147,9 +196,16 @@ function andGateButtonClick() {
 function orGateButtonClick() {
   addOrGate(0, 0);
   stage.add(layer);
+  console.log(connectorX, connectorY);
+}
+
+function connectorButtonClick() {
+  addConnectorButton(0, 0);
+  stage.add(layer);
 }
 
 window.trueButtonClick = trueButtonClick;
 window.falseButtonClick = falseButtonClick;
 window.andGateButtonClick = andGateButtonClick;
 window.orGateButtonClick = orGateButtonClick;
+window.connectorButtonClick = connectorButtonClick;
